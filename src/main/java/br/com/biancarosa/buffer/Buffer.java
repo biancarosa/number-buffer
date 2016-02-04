5eca4c6d9a937e67f910fb43dfa9a13fc5699a97
Bianca Rosa <me@biancarosa.com.br>
2016-02-04 10:33:36 -0200

    Start project

---
+++ src/main/java/br/com/biancarosa/buffer/Buffer.java
@@ -0,0 +1,17 @@
+package br.com.biancarosa.buffer;
+
+import java.util.Deque;
+import java.util.concurrent.LinkedBlockingDeque;
+
+public class Buffer {
+
+    int max;
+    Deque<Integer> numbers;
+
+    public Buffer(int max) {
+        this.max = max;
+        this.numbers = new LinkedBlockingDeque<Integer>(max);
+    }
+
+
+}
---
+++ src/main/java/br/com/biancarosa/buffer/Executor.java
@@ -0,0 +1,9 @@
+package br.com.biancarosa.buffer;
+
+public class Executor {
+
+    public static void main(String[] args) {
+
+    }
+
+}
