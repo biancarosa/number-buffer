da6315d670dc72743622bd641f0831ffc1494e92
Bianca Rosa <me@biancarosa.com.br>
2016-02-04 10:41:47 -0200

    Starting socket code

--- src/main/java/br/com/biancarosa/buffer/Executor.java
+++ src/main/java/br/com/biancarosa/buffer/Executor.java
@@ -1,9 +1,38 @@
 package br.com.biancarosa.buffer;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class Executor {
 
     public static void main(String[] args) {
+        int port = 8000;
+        try {
+            port = Integer.parseInt(args[0]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.err.println("Wrong number of args provided");
+            System.exit(1);
+        }
 
+    /*    try {
+          ServerSocket serverSocket = new ServerSocket(port);
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                out.println(inputLine);
+            }
+        } catch (IOException e) {
+            System.out.println("Exception caught when trying to listen on port "
+                    + portNumber + " or listening for a connection");
+            System.out.println(e.getMessage());
+        }*/
     }
 
 }
