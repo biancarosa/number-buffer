5c118b0842c34746e11fedb7c174ca94fd3fcfb8
Bianca Rosa <me@biancarosa.com.br>
2016-02-09 00:38:31 -0200

    progress towards socket logic

--- src/main/java/br/com/biancarosa/buffer/Executor.java
+++ src/main/java/br/com/biancarosa/buffer/Executor.java
@@ -19,20 +19,22 @@ public class Executor {
             System.exit(1);
         }
 
-    /*    try {
-          ServerSocket serverSocket = new ServerSocket(port);
-            Socket clientSocket = serverSocket.accept();
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-                out.println(inputLine);
+        System.out.println("Running on port " + port);
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    System.out.println(inputLine);
+                }
             }
         } catch (IOException e) {
-            System.out.println("Exception caught when trying to listen on port "
-                    + portNumber + " or listening for a connection");
             System.out.println(e.getMessage());
-        }*/
+        }
+
     }
 
 }
